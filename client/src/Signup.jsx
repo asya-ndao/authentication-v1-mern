e2801e28a2637376314d0070f29791478b8cf112
asya-ndao <sene.asya@gmail.com>
2025-05-01 17:47:49 +0100

    update: Add useNavigate for Login

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios";
 
 const Signup = () => {
   const [name, setName] = useState()
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    axios.post("", {name, email, password})
-    .then(result => console.log(result))
+    axios.post("http://localhost:3001/register", {name, email, password})
+    .then(result => {console.log(result)
+      navigate("/login")
+    })
     .catch(err => console.log(err))
   }
 
