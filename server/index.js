5f59b0300731a47a11a462e38681e3eaa708d972
asya-ndao <sene.asya@gmail.com>
2025-05-01 17:50:28 +0100

    update: Connect mongodb to the backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,37 @@
-import express  from "express";
+import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+
+import { Employee } from "./models/Employee.js"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017")
+mongoose.connect("mongodb://127.0.0.1:27017/employee")
+
+app.post("/login", (req,res) => {
+    const {email, password} = req.body
+    Employee.findOne({email:email})
+    .then(user => {
+        if (user) {
+            if (user.password === password) {
+                res.json("Success")
+            } else {
+                res.json("the password is incorrect")
+            }
+        } else {
+            res.json("No record existed.")
+        }
+    })
+})
+
+app.post("/register", (req, res) => {
+    Employee.create(req.body)
+        .then(employees => res.json(employees))
+        .catch(err => res.json(err))
+})
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
